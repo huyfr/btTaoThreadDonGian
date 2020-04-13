30c993a9575da554bcee1a5bf2ba86653dc9bfff
huyfr <61008601+huyfr@users.noreply.github.com>
2020-04-13 13:46:54 +0700

    Bài tập tạo thread đơn giản

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        Thread thread1 = new Thread(numberGenerator);
+        Thread thread2 = new Thread(numberGenerator);
+
+        thread1.start();
+        thread2.start();
+    }
+}
